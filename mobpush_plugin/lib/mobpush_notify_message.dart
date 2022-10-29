e8ce82e3833843d3c9d5bf1fe26c7c8fa53f8c1a
lucky1213 <lucky1213@vip.qq.com>
2022-10-29 17:13:49 +0800

    add huawei parse

--- mobpush_plugin/lib/mobpush_notify_message.dart
+++ mobpush_plugin/lib/mobpush_notify_message.dart
@@ -1,42 +1,68 @@
-
 class MobPushNotifyMessage {
 // iOS Properties
-	String? subTitle;
-	String? sound;
-	int? badge;
+  String? subTitle;
+  String? sound;
+  int? badge;
 // Android Properties
-	String? styleContent;
-	String? messageId;
-	List<String>? inboxStyleContent;
-	int? style;
-	int? channel;
-	Map<String, dynamic>? extrasMap;
-	bool? voice;
-	bool? shake;
-	bool? light;
-	String? title;
-	String content;
-	int? timestamp;
+  String? styleContent;
+  String? messageId;
+  List<String>? inboxStyleContent;
+  int? style;
+  int? channel;
+  Map<String, dynamic>? extrasMap;
+  bool? voice;
+  bool? shake;
+  bool? light;
+  String? title;
+  String content;
+  int? timestamp;
 
-	MobPushNotifyMessage({this.title, required this.content, this.timestamp, this.subTitle, this.sound, this.badge, this.styleContent, required this.messageId, this.inboxStyleContent, this.style, this.channel, this.extrasMap, this.voice, this.shake, this.light});
+  MobPushNotifyMessage(
+      {this.title,
+      required this.content,
+      this.timestamp,
+      this.subTitle,
+      this.sound,
+      this.badge,
+      this.styleContent,
+      required this.messageId,
+      this.inboxStyleContent,
+      this.style,
+      this.channel,
+      this.extrasMap,
+      this.voice,
+      this.shake,
+      this.light});
 
-	factory MobPushNotifyMessage.fromJson(Map<String, dynamic> json) {
-		return MobPushNotifyMessage(
-                  title: json['title'],
-                  content: json['content'],
-                  messageId: json['messageId'],
-                  timestamp: json['timestamp'],
-                  style: json['style'],
-                  channel: json['channel'],
-                  voice: json['voice'],
-                  shake: json['shake'],
-                  extrasMap: json['extrasMap'],
-                  inboxStyleContent: json['inboxStyleContent'],
-                  styleContent: json['styleContent'],
-                  light: json['light'],
-                  badge: json['badge'],
-                  sound: json['sound'],
-                  subTitle: json['subTitle']);
-	}
+  factory MobPushNotifyMessage.fromJson(Map<String, dynamic> json) {
+    return MobPushNotifyMessage(
+      title: json['title'],
+      content: json['content'] ?? '',
+      messageId: json['messageId'],
+      timestamp: json['timestamp'],
+      style: json['style'],
+      channel: json['channel'],
+      voice: json['voice'],
+      shake: json['shake'],
+      extrasMap: json.containsKey('extra') ? json['extra'] : json['extrasMap'],
+      inboxStyleContent: json['inboxStyleContent'],
+      styleContent: json['styleContent'],
+      light: json['light'],
+      badge: json['badge'],
+      sound: json['sound'],
+      subTitle: json['subTitle'],
+    );
+  }
 
+  factory MobPushNotifyMessage.fromHuawei(Map<String, dynamic> json) {
+    return MobPushNotifyMessage(
+      content: json['content'] ?? '',
+      messageId: null,
+      extrasMap: json.containsKey('pushData')
+          ? {
+              'pushData': json['pushData'],
+            }
+          : null,
+    );
+  }
 }
